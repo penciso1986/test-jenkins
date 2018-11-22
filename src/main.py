fc2269fec398745988613afafe1751317a29d77d
Pablo Enciso <pablorodrigo.enciso@gmail.com>
2018-11-21 23:42:15 -0300

    Ficheros scripts & src

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("7")
+def resta(a,b):
+	return a-b
+
+@app.route("/")
 def hello():
-	return "Hello World"
+	res = resta(10,20)
+	return "Hello World %s" % (res)
 
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
+	app.run(host="0.0.0.0", port=5000)
---
+++ src/test_main.py
@@ -0,0 +1,4 @@
+from main import *
+
+def test_resta():
+	assert resta(10,20) == -10
